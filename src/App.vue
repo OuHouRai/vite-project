4fe25877984795366638bb7d3df64b73c62c384a
wpl1874 <wpl1874@yahoo.co.jp>
2024-08-31 00:15:31 +0900

    Ver1.00

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,11 @@
 </script>
 
 <template>
-  <div>
+  <div class="Test">
     test
   </div>
 </template>
 
-<style scoped>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-
-  .logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-  }
+<style lang="less" scoped>
+  @import "./styles/Test.less";
 </style>
